602b34dcbdecc42e9a16806c1e9ae16eeec67cd4
Suhel <suhel7822@gmail.com>
2024-10-15 22:07:44 +0530

    supase and prisma connection implementation

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,18 +1,52 @@
 "use server";
 
 import { profileSchema } from "@/utils/schema";
+import prisma from "@/lib/db";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+
+    if (!user) {
+      throw new Error("Please login to create profile.");
+    }
+
     const rowData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rowData);
-    console.log(validatedFields);
-    return { message: "Profile validated" };
+
+    // creating user profile
+    await prisma.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl || "",
+        ...validatedFields,
+      },
+    });
+
+    // for syncinc clerk and webapp
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+
+    // return response for toast
   } catch (err) {
-    console.error(err);
-    return { message: "There was an error." };
+    return {
+      message:
+        err instanceof Error
+          ? err.message
+          : "Could not process your request at the moment💥💥💥",
+    };
   }
+
+  // redirecting user to home page
+  redirect("/");
 };
